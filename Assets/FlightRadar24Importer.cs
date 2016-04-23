b01009a789b3d826b1b59b7fe7fefbf938300909
satdenis <satellite.sfd@gmail.com>
2016-04-23 18:49:50 +0300

    refactored FlightRadar24Importer

--- Assets/FlightRadar24Importer.cs
+++ Assets/FlightRadar24Importer.cs
@@ -11,11 +11,16 @@ public class FlightRadar24Importer : AirtcraftImporter {
 	
 	public override IEnumerable Import()
 	{
-		var result = GetIdFromJson ("{\"97f6906\":[\"8963E5\",60.9580,17.6808,134,35000,490,\"2540\",\"F-ESSD2\",\"A388\",\"A6-EOH\",1461402984,\"SFO\",\"DXB\",\"EK226\",0,64,\"UAE226\",0]}");
+		var result = GetAircraftsFromJson ("{\"97f6906\":[\"8963E5\",60.9580,17.6808,134,35000,490,\"2540\",\"F-ESSD2\",\"A388\",\"A6-EOH\",1461402984,\"SFO\",\"DXB\",\"EK226\",0,64,\"UAE226\",0]}");
 		return null;
 	}
 
-	private List<Aircraft> GetIdFromJson(string json)
+	/// <summary>
+	/// Gets the aircrafts from json.
+	/// </summary>
+	/// <returns>The aircrafts from json.</returns>
+	/// <param name="json">Json as web response from flight radar</param>
+	private List<Aircraft> GetAircraftsFromJson(string json)
 	{
 		JSONClass response = (JSONClass)JSON.Parse (json);
 
@@ -24,7 +29,7 @@ public class FlightRadar24Importer : AirtcraftImporter {
 		foreach (var responseEntry in response) {
 			var entry = (KeyValuePair<string, JSONNode>)responseEntry;
 			if (Regex.IsMatch (entry.Key, @"^\d")) {
-				JSONArray mydata = (JSONArray)value.Value;
+				JSONArray mydata = (JSONArray)entry.Value;
 
 				var lat = float.Parse(mydata [1].ToString ().Replace("\"", ""));
 				var lon = float.Parse(mydata [2].ToString ().Replace("\"", ""));
