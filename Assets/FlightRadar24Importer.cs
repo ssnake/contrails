817ac595e618abc83cf51684dc55a097f99e5b15
ssnake <max.nedelchev@gmail.com>
2016-04-24 09:09:52 +0300

    got rid of simplejson

--- Assets/FlightRadar24Importer.cs
+++ Assets/FlightRadar24Importer.cs
@@ -1,11 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using Newtonsoft.Json;
-//using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Text.RegularExpressions;
-using SimpleJSON;
 
 public class FlightRadar24Importer : AirtcraftImporter {
     DistPoint distPoint;
@@ -47,39 +44,7 @@ public class FlightRadar24Importer : AirtcraftImporter {
         return list;
 
     }
-    /// <summary>
-    /// Gets the aircrafts from json.
-    /// </summary>
-    /// <returns>The aircrafts from json.</returns>
-    /// <param name="json">Json as web response from flight radar</param>
-    private List<Aircraft> GetAircraftsFromJson(string json)
-	{
-        Debug.Log("Parsing: " + json);
-		JSONClass response = (JSONClass)JSON.Parse (json);
-
-		var planes = new List<Aircraft> ();
-
-		foreach (var responseEntry in response) {
-			var entry = (KeyValuePair<string, JSONNode>)responseEntry;
-			if (Regex.IsMatch (entry.Key, @"^\d")) {
-				JSONArray mydata = (JSONArray)entry.Value;
-
-				var lat = float.Parse(mydata [1].ToString ().Replace("\"", ""));
-				var lon = float.Parse(mydata [2].ToString ().Replace("\"", ""));
-				var alt = float.Parse(mydata [4].ToString ().Replace("\"", "")) * 0.3048f;
-				var pointId = entry.Key.Replace("\"", " ");
-
-				//international flight identificator
-				//var flightId = mydata [13].ToString ().Replace("\"", "");
-
-				var plane = new Aircraft (pointId, lon, lat, alt);
-
-				planes.Add (plane);
-			}
-		}
-
-		return planes;
-	}
+    
     private void CalculateWaypoints(List<Aircraft> planes)
     {
         foreach (var plane in planes)
@@ -115,57 +80,7 @@ public class FlightRadar24Importer : AirtcraftImporter {
 
         
     }
-    private void UpdateWaypoint(Aircraft plane, string json)
-    {
-        JSONClass response = (JSONClass)JSON.Parse(json);
-
-        List<Waypoint> points = new List<Waypoint>();
-
-        foreach (var responseEntry in response)
-        {
-            var entry = (KeyValuePair<string, JSONNode>)responseEntry;
-            if (entry.Key.Contains("trail"))
-            {
-                JSONArray mydata = (JSONArray)entry.Value;
-
-                var children = mydata.Children;
-                //var history = (JSONClass)mydata [0];
-
-                Waypoint wayPoint = new Waypoint(0, 0, 0);
-
-                foreach (var line in children)
-                {
-                    
-                    
-                    foreach (var kvp in (JSONClass) line)
-                    {
-                        var pair = (KeyValuePair<string, JSONNode>)kvp;
-                        var key = pair.Key;
-                        var val = pair.Value.ToString();
-
-                        switch (key)
-                        {
-                            case "lat":
-                                wayPoint.latitude = float.Parse(val);
-                                break;
-
-                            case "lng":
-                                wayPoint.longitude = float.Parse(val);
-                                break;
-
-                            case "alt":
-                                wayPoint.altitude = float.Parse(val);
-                                break;
-                        }
-
-                        points.Add(wayPoint);
-                    }
-                }
-            }
-        }
-
-        plane.route = points;
-    }
+    
     string GetData(string url)
     {
         
